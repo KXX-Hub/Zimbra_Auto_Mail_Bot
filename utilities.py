f44a5f7738064b960e2db6678739fe7e6fa84e05
GSS_DOMAIN\kai_hung <kai_hung@gss.com.tw>
2023-09-28 16:36:10 +0800

    Update - edit the config msg

--- utilities.py
+++ utilities.py
@@ -9,8 +9,9 @@ from yaml import SafeLoader
 def config_file_generator():
     """Generate the template of config file"""
     with open('config.yml', 'w', encoding="utf8") as f:
-        f.write("""# | Zimbra_Auto_Mail_Bot               |
-# | Made by KXX                          |
+        f.write("""# ++------------------------------------++
+# | Zimbra_Auto_Mail_Bot               |
+# | Made by KXX                        |
 # ++------------------------------------++
 
 # Your email account.
